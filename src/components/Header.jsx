00437b1e0db63bc531dc0e837f3437469c5da718
Yhethi <Yhethi@users.noreply.github.com>
2025-03-11 13:42:49 -0400

    Updated: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
 
-  const questions = Array.from({ length: 7 }, (_, i) => `question${i + 1}`);
+  const questions = Array.from({ length: 2 }, (_, i) => `question${i + 1}`);
 
   return (
     <div className="global_navbar">
